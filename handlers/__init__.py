a0c185d41814f9d9f6808629d63a3db47514a1c8
senanshabaan1 <senanshabaan2@gmail.com>
2026-02-24 00:52:24 +0300

    __init__.py

--- handlers/__init__.py
+++ handlers/__init__.py
@@ -4,11 +4,13 @@ from .services import router as services_router
 from .deposit import router as deposit_router
 from .admin import router as admin_router
 from .reports import router as reports_router
+from .profile import router as profile_router  # 👈 أضفنا هذا لأن الملف موجود
 
-___all__ = [
-    'start_router', 
-    'services_router', 
-    'deposit_router', 
+__all__ = [
+    'start_router',
+    'services_router',
+    'deposit_router',
     'admin_router',
-    'reports_router'  # 👈 وأضف هنا كمان
+    'reports_router',
+    'profile_router',  # 👈 أضفنا هذا
 ]
