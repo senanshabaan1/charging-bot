0d9b57c8af5c0024c7a38e5c1e90a8c1499baf48
senanshabaan1 <senanshabaan2@gmail.com>
2026-03-09 23:45:36 +0300

    deposit.py

--- handlers/deposit.py
+++ handlers/deposit.py
@@ -8,7 +8,7 @@ import asyncio
 import logging
 from handlers.time_utils import get_damascus_time_now, format_damascus_time, DAMASCUS_TZ
 from datetime import datetime
-from handlers.keyboards import get_main_menu_keyboard, get_main_menu_only_inline_keyboard
+from handlers.keyboards import get_main_menu_keyboard, get_main_menu_only_keyboard
 from database.users import is_admin_user
 from database.core import get_exchange_rate, get_syriatel_numbers
 from utils import get_formatted_damascus_time, format_amount, is_valid_positive_number, parse_number
